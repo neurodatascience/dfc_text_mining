a0c065d128f5e97e97c0be5ed9ef6a6d1e597878
Jerome Dockes <jerome@dockes.org>
2022-12-13 11:50:00 -0300

    use /citations/ route instead of /metadata/ bc /metadata/ doesn't work
    metadata would be more efficient, getting citations for multiple papers in one
    request, but it just returns a 500 internal server error after a long time

--- collect_citations.py
+++ collect_citations.py
@@ -1,20 +1,16 @@
-import json
 import os
 import pathlib
 
 import requests
 
 # see https://opencitations.net/index and https://opencitations.net/index/api/v1
-OPENCITATIONS_URL = "https://w3id.org/oc/index/api/v1/metadata/"
+# "references" for cited papers "citations" for citing papers
+OPENCITATIONS_URL = "https://opencitations.net/index/coci/api/v1/citations/"
 HEADERS = {}
 if "OPENCITATIONS_ACCESS_TOKEN" in os.environ:
     HEADERS["authorization"] = os.environ["OPENCITATIONS_ACCESS_TOKEN"]
 
 
 all_dois = pathlib.Path("dfc_dois.txt").read_text("UTF-8").strip().split("\n")
-citations = requests.get(
-    f"{OPENCITATIONS_URL}{'__'.join(all_dois)}", headers=HEADERS
-)
-pathlib.Path("opencitations_data.json").write_text(
-    json.dumps(citations.json()), encoding="UTF-8"
-)
+citations = requests.get(f"{OPENCITATIONS_URL}{all_dois[0]}", headers=HEADERS)
+print(citations.json())
